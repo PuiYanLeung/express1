f1d1f0932fb6440679b0dd7c252a82035add6e4e
PuiYanLeung <trewina@gmail.com>
2021-08-23 11:22:37 +0100

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+const port = 80;
+
+app.get("/", (req, res) =>{
+    res.status(200).send("Hello world");
+});
+
+app.listen(port, ()=>{
+    console.log("App is online");
+});

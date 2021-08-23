959a8ac9bc22d95e71494fe7b95078f8380894bc
PuiYanLeung <trewina@gmail.com>
2021-08-23 12:28:38 +0100

    query param

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
-const port = 80;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) =>{
     res.status(200).send("Hello world");
+});
+
+app.get("/about", (req, res) =>{
+    res.status(200).send("This is the about route");
+});
+
+// http://localhost/users/nmunro?age=3456
+app.get("/users/:username", (req, res) =>{
+    console.log(req.query);
+    res.status(200).send(`You requested information about ${req.params.username}:${req.query.age}`);
+});
+
+app.get("/:username/:project", (req, res) =>{
+    res.status(200).send(`You requested information about ${req.params.project} created by ${req.params.username}`);
 });
 
 app.listen(port, ()=>{
